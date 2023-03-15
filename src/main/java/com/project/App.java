b8f4a8d923fe82af527884d720101e2fdbb474f2
pavithra200595 <pavipavi491@gmail.com>
2023-03-15 14:54:34 +0530

    First commit

---
+++ src/main/java/com/project/App.java
@@ -0,0 +1,14 @@
+package com.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("This is my first maven project");
+    }
+}
